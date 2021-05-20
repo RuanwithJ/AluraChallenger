bef3cd6aaefce3af258065f8796a1458605a6394
juanC <juan.carlos.56@usp.br>
2021-05-19 22:30:41 -0300

    versao mobile, entre outras mudanças

--- js/mudarCor.js
+++ js/mudarCor.js
@@ -1,7 +1,17 @@
 const seletorDeCores = document.querySelector('#colors');
-seletorDeCores.classList.add('blue');
+var cores = ['rgb(0,0,255)','rgb(255, 153, 153)', 'rgb(102, 204, 255)','rgb(153, 255, 204)',
+'rgb(153, 255, 102)','rgb(255, 255, 102)', 'rgb(255, 153, 204)']
+function criaPalheta(select, cores){
+    cores.forEach(cor => {
+        const novaCor = document.createElement('option');
+        novaCor.style.backgroundColor = cor;
+        select.appendChild(novaCor);
+    });
+};
+criaPalheta(seletorDeCores, cores);
+seletorDeCores.style.backgroundColor = cores[0];
 seletorDeCores.addEventListener('click', function(){
-    const color = seletorDeCores.options[seletorDeCores.selectedIndex].text;
+    const color =  cores[seletorDeCores.selectedIndex]
     seletorDeCores.classList = [];
-    seletorDeCores.classList.add(color);
+    seletorDeCores.style.backgroundColor = color;
 });
